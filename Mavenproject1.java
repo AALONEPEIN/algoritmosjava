5ff3f4727dc8ed3e90a748b598f385f74fc0d2c4
Aaron Sanchez Aalone yagami <69217141+AALONEPEIN@users.noreply.github.com>
2022-10-02 00:15:15 -0500

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author AsusMauro
+ */
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+       
+        
+                  for(int n=1;n<100;n++)      {                                      
+         System.out.println("Tabla del " + n);
+        for(int i = 1; i<=10; i++){
+          
+             System.out.println(n + " * " + i + " = " + n*i);  
+             
+    }
+}
+}
+}
